4b3bdebd0b7d66493a21ed7a50c4c1dc92a45390
Iuri Kintschev <jtc.iuri07@gmail.com>
2020-09-10 07:15:19 -0700

    Update apolloClient.ts

--- src/services/apolloClient.ts
+++ src/services/apolloClient.ts
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "https://graphql-pokemon.now.sh",
+  uri: "https://graphql-pokemon2.vercel.app/",
 });
 
 export default client;
